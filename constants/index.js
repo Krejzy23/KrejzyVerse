267ef80d34f7e1385c53ca167ac027f519136d4d
Aleš Krejzl <ales.krejzl@Ales-MacBook-Air.local>
2024-02-20 15:21:51 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,93 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'QuantumVerse',
+    description: 'A digital universe where quantum technology opens up endless possibilities for virtual reality, interaction and discovery.',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'NexaRealm',
+    description:'A world where magic and technology merge, offering immersion in vast, vibrant landscapes full of adventure and mystery.',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'AetherZone',
+    description: 'A celestial archipelago where aether currents set the rules of reality, connecting myriad worlds in a kaleidoscope of adventure and discovery.',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'EtherealNet',
+    description: 'A virtual network connected by invisible threads of energy, allowing instantaneous travel between worlds of dreams and thoughts.'
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'VirtualDawn',
+    description: 'The dawn of a new era in the digital universe, where every sunrise reveals endless possibilities for exploration, creation and connection.',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading Grimoire to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title:`You'll never want to leave this metaverse for the real world!`,
+    subtitle:
+        'Immerse yourself in a digital paradise where your dreams come to life in an infinite universe of possibilities and adventures that transcend reality.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Unlock the secrets of virtual worlds with these key tips for navigating, creating and interacting in an endless digital universe.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Explore every corner of the planet from the comfort of your home with technology that bridges the boundaries between reality and digital discovery.'
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
